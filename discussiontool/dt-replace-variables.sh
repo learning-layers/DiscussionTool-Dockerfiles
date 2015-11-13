a6cbc09b7c18630cd626df88e00a82d781de6754
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-11-13 15:29:28 +0200

    Fixed config replace, using correct URI and locations.
    The last remaining issue should be the automatic OIDC client
    registration and identifier provision.

--- discussiontool/dt-replace-variables.sh
+++ discussiontool/dt-replace-variables.sh
@@ -4,10 +4,10 @@
 cd /usr/share/nginx/html/scripts
 
 # Replace configurations as needed
-sed -i "s#OIDC_AUTHORIZATION_URL#${BASE_URL}/o/oauth2/authorize#g" scripts.*.js
+sed -i "s#OIDC_AUTHORIZATION_URL#${LAYERS_API_URI}o/oauth2/authorize#g" scripts.*.js
 sed -i "s#OIDC_CLIENT_ID#NOT_IMPLEMENTED_YET#g" scripts.*.js
-sed -i "s#SSS_REST_URL#${BASE_URL}/sss#g" scripts.*.js
-sed -i "s#LD_REST_URL#${BASE_URL}/ldserver#g" scripts.*.js
-sed -i "s#LD_CLIENT_URL#${BASE_URL}/ldclient#g" scripts.*.js
-sed -i "s#BNP_URL#${BASE_URL}/bitsandpieces#g" scripts.*.js
+sed -i "s#SSS_REST_URL#${LAYERS_API_URI}sss.adapter.rest.v2/#g" scripts.*.js
+sed -i "s#LD_REST_URL#${LAYERS_API_URI}lds/#g" scripts.*.js
+sed -i "s#LD_CLIENT_URL#${LAYERS_API_URI}ldc/#g" scripts.*.js
+sed -i "s#BNP_URL#${LAYERS_API_URI}bitsandpieces/#g" scripts.*.js
 

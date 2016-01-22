b4be5065d0b116cdff757d50ef5d48d862ceda52
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-01-22 21:15:22 +0200

    Samll fix to lock file location.

--- discussiontool/dt-run.sh
+++ discussiontool/dt-run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Replace variables if needed
+initialScriptLocation=$PWD
 if [ ! -f dt-replacement-made.lock ];
 then
 	source ~/bin/dt-replace-variables.sh
+	cd $initialScriptLocation
 	touch dt-replacement-made.lock
 fi
 

1daa220ecc39228e1a4cb7b9246f0a1c7a656b3a
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-01-22 15:47:18 +0200

    Added OIDC client registration and variable replacement part.

--- discussiontool/dt-replace-variables.sh
+++ discussiontool/dt-replace-variables.sh
@@ -5,7 +5,7 @@ cd /usr/share/nginx/html/scripts
 
 # Replace configurations as needed
 sed -i "s#OIDC_AUTHORIZATION_URL#${LAYERS_API_URI}o/oauth2/authorize#g" scripts.*.js
-sed -i "s#OIDC_CLIENT_ID#NOT_IMPLEMENTED_YET#g" scripts.*.js
+sed -i "s#OIDC_CLIENT_ID#{DISCUSSIONTOOL_OIDC_CLIENT_ID}#g" scripts.*.js
 sed -i "s#SSS_REST_URL#${LAYERS_API_URI}sss.adapter.rest.v2/#g" scripts.*.js
 sed -i "s#LD_REST_URL#${LAYERS_API_URI}lds/#g" scripts.*.js
 sed -i "s#LD_CLIENT_URL#${LAYERS_API_URI}ldc/#g" scripts.*.js

92b5b158cd01920ed439753bc107d29b58e2b6ae
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-03-02 13:01:43 +0200

    Using release 2.0.0
    Also added an additional configuration placeholder to replacements.

--- discussiontool/dt-replace-variables.sh
+++ discussiontool/dt-replace-variables.sh
@@ -7,6 +7,7 @@ cd /usr/share/nginx/html/scripts
 sed -i "s#OIDC_AUTHORIZATION_URL#${LAYERS_API_URI}o/oauth2/authorize#g" scripts.*.js
 sed -i "s#OIDC_CLIENT_ID#{DISCUSSIONTOOL_OIDC_CLIENT_ID}#g" scripts.*.js
 sed -i "s#SSS_REST_URL#${LAYERS_API_URI}sss.adapter.rest.v2/#g" scripts.*.js
+sed -i "s#LD_DOCUMENT_BASE#${LAYERS_API_URI}lds/#g" scripts.*.js
 sed -i "s#LD_REST_URL#${LAYERS_API_URI}lds/#g" scripts.*.js
 sed -i "s#LD_CLIENT_URL#${LAYERS_API_URI}ldc/#g" scripts.*.js
 sed -i "s#BNP_URL#${LAYERS_API_URI}bitsandpieces/#g" scripts.*.js
